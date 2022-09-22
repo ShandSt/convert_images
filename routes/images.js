344dfd9422fd5adf40840e247edc61a6f42a80ba
shandst <shandstas@gmail.com>
2022-09-22 16:02:12 +0300

    upload file

--- routes/images.js
+++ routes/images.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, 'uploads/');
+  },
+  filename: function (req, file, callback) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    //callback(null, file.fieldname + '-' + uniqueSuffix);
+    callback(null , 'upload_at_' + uniqueSuffix + path.extname(file.originalname)); 
+  }
+});
 const upload = multer({
   dest: 'uploads/',
   limits: {
     fileSize: 1000000,
   },
+  storage: storage,
   fileFilter(req, file, cb) {
     if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
       cb(new Error('Please upload an image.'))
     }
     cb(undefined, true);
   }
-});
+}).single("file");
 
 const genres = [
   { id: 1, name: 'Action' },  
@@ -24,31 +37,27 @@ router.get('/', (req, res) => {
   res.send(genres);
 });
 
-router.post('/upload', upload.single('file'), async (req, res) => {
-    try {
-      console.log(req.sessionID)
-      // const incident = await Incident.findById(req.body.id);
-      // incident.image = req.file.buffer;
-      // incident.save();
-      res.send();
-    } catch (e){
-      console.log(222);
-      res.status(400).send(e);
-    }
+router.post('/upload',  async (req, res) => {
+      upload(req, res, (err) => {
+        if(err) {
+          res.status(400).send("Something went wrong!");
+        }
+          console.log(req);
+        try {
+          //console.log(req.sessionID)
+          // const incident = await Incident.findById(req.body.id);
+          // incident.image = req.file.buffer;
+          // incident.save();
+          //res.send('Complete');
+        } catch (e){
+          res.status(400).send(e);
+        }
+        res.send(req.file);
+      });
   }, (error, req, res, next) => {
     res.status(400).send({error: error.message});
 });
 
-
-
-const storage = multer.diskStorage({
-  destination: function(req, file, callback) {
-    callback(null, '/src/images');
-  },
-  filename: function (req, file, callback) {
-    callback(null, file.fieldname);
-  }
-});
 
 
 
